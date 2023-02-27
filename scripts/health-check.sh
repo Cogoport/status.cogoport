aace189b93548eaed4947a7b7b92979869d082f6
Sai Tarun <sanagapati.tarun@cogoport.com>
2023-02-27 14:52:34 +0530

    back to normal

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -89,7 +89,7 @@ do
       ]
   }'
   }
-  if [ "$result" = "success" ]
+  if [ "$result" = "failed" ]
   then
     curl -H 'Content-Type: application/json' -d "$(get_post_data "Shivom Mahar" shivom.mahar@cogoport.com)" $TEAMS_WEBHOOK_URL &> /dev/null
   fi
