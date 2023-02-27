1a2306c33022ffe64a6a988ffb79ad66246aca04
Sai Tarun <sanagapati.tarun@cogoport.com>
2023-02-27 17:44:15 +0530

    fix:`
    Signed-off-by: Sai Tarun <sanagapati.tarun@cogoport.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
     return (
@@ -7,7 +8,7 @@ export default function Document() {
             <body className="dark:bg-gray-800">
                 <Main />
                 <NextScript />
-                <script src="https://cdn.tailwindcss.com"></script>
+                <Script src="https://cdn.tailwindcss.com"/>
             </body>
         </Html>
     )
